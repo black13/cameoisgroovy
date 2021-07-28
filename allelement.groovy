6bf5642d0ac07218328581d6bd5f328a1b9f9182
black13 <jjosburn@gmail.com>
2021-07-27 19:19:00 -0600

    allelement.groovy

--- allelement.groovy
+++ allelement.groovy
@@ -1,17 +1,22 @@
-import com.nomagic.magicdraw.core.Application
-import com.nomagic.magicdraw.core.GUILog
-import com.nomagic.magicdraw.core.Project
-import com.nomagic.magicdraw.openapi.uml.SessionManager
-import com.nomagic.magicdraw.uml.Finder
-import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class
-import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element
-                                                        
+import com.nomagic.magicdraw.core.Application;
+import com.nomagic.magicdraw.core.GUILog;
+import com.nomagic.magicdraw.core.Project;
+import com.nomagic.magicdraw.openapi.uml.SessionManager;
+import com.nomagic.magicdraw.uml.Finder;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
+import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
+
 try {
     SessionManager.getInstance().createSession("session")
     project = Application.getInstance().getProject();
 
-    models = Finder.byNameAllRecursively().find(project.getPrimaryModel(), Model.class, "MyModel");
-
+    //models = Finder.byNameAllRecursively().find(project.getPrimaryModel(), Class, "name1");
+    classes = Finder.byTypeRecursively().find(project.getPrimaryModel(),Class);
+    //give the name of each element.
+    for(Element classElem : classes) {
+ 
+    }
 }
 
 finally
