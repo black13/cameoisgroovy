8647dd3cef61650a81b434f4128dc190d13a93b0
black13 <jjosburn@gmail.com>
2021-08-08 10:13:00 -0600

    push for nick

--- allelement.groovy
+++ allelement.groovy
@@ -7,6 +7,34 @@ import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 
+// 
+import com.nomagic.magicdraw.core.Application;
+import com.nomagic.magicdraw.openapi.uml.ModelElementsManager;
+import com.nomagic.magicdraw.openapi.uml.PresentationElementsManager;
+import com.nomagic.magicdraw.openapi.uml.ReadOnlyElementException;
+import com.nomagic.magicdraw.sysml.util.SysMLConstants;
+import com.nomagic.requirements.util.SysMLRequirementConstants;
+import com.nomagic.magicdraw.ui.dialogs.MDDialogParentProvider;
+import com.nomagic.magicdraw.uml.symbols.DiagramPresentationElement;
+import com.nomagic.magicdraw.uml.symbols.PresentationElement;
+import com.nomagic.magicdraw.uml.symbols.shapes.DiagramPropertiesShape;
+import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Diagram;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Namespace;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
+import com.nomagic.uml2.ext.magicdraw.mdprofiles.Stereotype;
+
+
+import com.nomagic.uml2.impl.ElementsFactory;
+import com.nomagic.magicdraw.properties.BooleanProperty;
+import com.nomagic.magicdraw.properties.PropertyID;
+
+import com.nomagic.magicdraw.properties.PropertyManager;
+import com.nomagic.uml2.ext.magicdraw.mdprofiles.Profile;
+import com.nomagic.magicdraw.uml.symbols.shapes.ShapeElement;
+
 try {
     SessionManager.getInstance().createSession("session")
     project = Application.getInstance().getProject();
